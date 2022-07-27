f46896c7a57a180decb7512daa196cbb9109af7a
Yoosuf <haffejeeyoosuf1@gmail.com>
2022-07-27 15:34:49 +0200

    init

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+class snake{
+    constructor(name, headCol, BodyCol, headX, headY, parts, tailLength, xVelocity, yVelocity, score){
+        this.name=name;
+        this.headCol=headCol;
+        this.BodyCol=BodyCol;
+        this.headX=headX;
+        this.headY=headY;
+        this.parts=parts;
+        this.tailLength=tailLength;
+        this.xVelocity=xVelocity;
+        this.yVelocity=yVelocity;
+        this.score=score;
+    }
+}
+
 class snakePart{
     constructor(x, y){
         this.x=x;
@@ -13,40 +28,32 @@ const ctx=canvas.getContext('2d');
 let sameTeam = false;
 let TouchControls = false;
 let speed = 12;
-let canvasSize = 400;
+let canvasSize = 600;
 //GameOver
 let lives = 1;
 let solidWalls = false;
 let WinningScore = 25;
+//Players
 //Color
 let snake1HeadColor = "orange";
 let snake1BodyColor = "green";
 let snake2HeadColor = "yellow";
 let snake2BodyColor = "blue";
 
+let custom1 = [];
+let custom2 = [];
 canvas.width = canvasSize;
 canvas.height = canvasSize;
-let tileCount = canvasSize/20;
+let tileCount = canvasSize/24;
 
-let tileSize=canvas.clientWidth/tileCount-2;
+let tileSize=canvasSize/tileCount;
 
-let headX=5;
-let headY=10;
+let snake1 = new snake("Player1", snake1HeadColor, snake1BodyColor, 5, 10, [], 1, 0, 0, 0);
+let snake2 = new snake("Player2", snake2HeadColor, snake2BodyColor, 15, 10, [], 1, 0, 0, 0);
+let snake3 = new snake("Player3", "purple", "lime", 5, 5, [], 1, 0, 0, 0);
+let snake4 = new snake("Player4", "pink", "cyan", 15, 5, [], 1, 0, 0, 0);
 
-let head2X=15;
-let head2Y=10;
-
-// array for snake parts
-const snakeParts=[];
-const snake2Parts=[];
-let tailLength=1;
-let tail2Length=1;
-
-//initialize the speed of snake
-let xvelocity=0;
-let yvelocity=0;
-let xvelocity2=0;
-let yvelocity2=0;
+let snakes = [snake1, snake2];
 
 //draw apple
 let appleX;
@@ -61,14 +68,10 @@ let AppleInitialized = false;
 
 // create game loop-to continously update screen
 function drawGame(){
-    changeSnakePosition();
-    changeSnake2Position();
-    
-    // game over logic
-    let stop = isGameOver();
-    if(stop === true){
-        return;
-    }
+
+    snakes.forEach(snake => {
+        moveSnake(snake);
+    });
     
     if(started && !AppleInitialized)
     {
@@ -78,213 +81,232 @@ function drawGame(){
     }
     
     clearScreen();
-    
-    drawSnake();
-    drawSnake2();
-    
+
+    snakes.forEach(snake => {
+        drawSnake(snake);
+    });
+
+    // game over logic
+    let stop = isGameOver();
+    if(stop === true){
+        return;
+    }
+
     drawApple();
-    checkCollision();
     
-    drawScore();
-    drawScore2();
+    snakes.forEach(snake => {
+        checkCollision(snake);
+        drawScore(snake);
+    });
     setTimeout(drawGame, 1000/speed);// Faster speed, less timeout
 }
 
 //Game Over function
 function isGameOver(){
-    let gameOver=false; 
-    //check whether game has started
-    if(yvelocity===0 && xvelocity===0 && yvelocity2===0 && xvelocity2===0){
-        return false;
-    }
+
+    let gameOver=false;
+
+    snakes.forEach(snake => {
+
+        //check whether game has started
+        if (snake.yVelocity === 0 && snake.xVelocity === 0) {
+            return false;
+        }
+
+        let otherSnakes = [];
+        otherSnakes.push(snakes);
+        console.log("before", otherSnakes);
+        let snakeIndex = snakes.indexOf(snake);
+        otherSnakes.splice(snakeIndex, 1);
+        console.log("after", otherSnakes);
+        debugger;
+
+        otherSnakes.forEach(otherSnake => {
+            // Game ends if snakes' heads clash
+            if((snake.headX === otherSnake.headX && snake.headY === otherSnake.headY) && started)
+            {
+                gameOver = true;
+            }
+        });
+
+        // only draw apple once game has started and both players moving
+        if (snake.yVelocity !== 0 || snake.xVelocity !== 0) {
+            started = true;
+        }
+
+        if(snake.score >= WinningScore)
+        {
+            gameOver=true;
+        }
+
+        if(solidWalls==true)
+        {
+            wallCollision(snake, gameOver);
+        }
+        else
+        {
+            wallTeleport(snake);
+        }
     
-    // only draw apple once game has started and both players moving
-    if((yvelocity!==0 || xvelocity!==0) && (yvelocity2!==0 || xvelocity2!==0)){
-    	started = true;
-    }
-    
-    if(score >= WinningScore || score2 >= WinningScore)
-    {
-    	gameOver=true;
-    }
-    
-    // Game ends if snakes' heads clash
-    if((headX == head2X && headY == head2Y) && started)
-    {
-    	gameOver = true;
-    }
-    
-    if(solidWalls==true)
-    {
-    	if(headX<0 || head2X<0){//if snake hits left wall
-    	gameOver=true;
-	    }
-	    else if(headX===tileCount || head2X===tileCount){//if snake hits right wall
-	        gameOver=true;
-	    }
-	    else if(headY<0 || head2Y<0){//if snake hits wall at the top
-	        gameOver=true;
-	    }
-	    else if(headY===tileCount || head2Y===tileCount){//if snake hits wall at the bottom
-	        gameOver=true;
-	    }
-    }
-    else
-    {
-		if(headX<0){//if snake hits left wall
-    		headX=tileCount;
-	    }
-	    else if(headX===tileCount){//if snake hits right wall
-	        headX=0;
-	    }
-	    else if(headY<0){//if snake hits wall at the top
-	        headY=tileCount;
-	    }
-	    else if(headY===tileCount){//if snake hits wall at the bottom
-	        headY=0;
-	    }
-	   
-	   // Snake2
-		if(head2X<0){//if snake hits left wall
-    		head2X=tileCount;
-	    }
-	    else if(head2X===tileCount){//if snake hits right wall
-	        head2X=0;
-	    }
-	    else if(head2Y<0){//if snake hits wall at the top
-	        head2Y=tileCount;
-	    }
-	    else if(head2Y===tileCount){//if snake hits wall at the bottom
-	        head2Y=0;
-	    }
-    }
-    
-    //stop game when snake crush to its own body
-     for(let i=0; i<snakeParts.length;i++){
-         let part=snakeParts[i];
-         if(part.x===headX && part.y===headY){//check whether any part of snake is occupying the same space
-             //gameOver=true;
-             let penalty = snakeParts.length - i - 1;
-             snakeParts.splice(snakeParts[i], penalty);
-             tailLength-=penalty;
-             score-= penalty;
-             break; // to break out of for loop
+        //stop game when snake crush to its own body
+         for(let i=0; i<snake.parts.length;i++){
+             let part=snake.parts[i];
+             if(part.x===snake.headX && part.y===snake.headY){//check whether any part of snake is occupying the same space
+                 //gameOver=true;
+                 let penalty = snake.parts.length - i - 1;
+                 snake.parts.splice(snake.parts[i], penalty);
+                 snake.tailLength-=penalty;
+                 snake.score-= penalty;
+                 break; // to break out of for loop
+             }
          }
-     }
-    
-    //stop game when snake2 crush to its own body
-     for(let i=0; i<snake2Parts.length;i++){
-         let part=snake2Parts[i];
-         if(part.x===head2X && part.y===head2Y){//check whether any part of snake is occupying the same space
-             //gameOver=true;
-             let penalty = snake2Parts.length - i - 1;
-             snake2Parts.splice(snake2Parts[i], penalty);
-             tail2Length-=penalty;
-             score2-= penalty;
-             break; // to break out of for loop
-         }
-     }
+    });
 
     //display text Game Over
     if(gameOver){
-     ctx.fillStyle="white";
-     ctx.font="50px verdana";
-     ctx.fillText("Game Over! ", canvas.clientWidth/6.5, canvas.clientHeight/2);//position our text in center
-     
-     if(score === score2)
-     {
-     	ctx.fillStyle="white";
-    	ctx.font="36px verdana";
-    	ctx.fillText("Draw!", canvas.clientWidth/2.5, canvas.clientHeight/2 + 50);
-     }
-     else if(score > score2)
-     {
-     	ctx.fillStyle=snake1HeadColor;
-    	ctx.font="36px verdana";
-    	ctx.fillText("Player 1 Wins!", canvas.clientWidth/5.5, canvas.clientHeight/2 + 50);
-     }
-     else
-     {
-     	ctx.fillStyle=snake2HeadColor;
-    	ctx.font="36px verdana";
-    	ctx.fillText("Player 2 Wins", canvas.clientWidth/5.5, canvas.clientHeight/2 + 50);
-     }
+        displayGameOverText(snakes, gameOver);
     }
 
-    return gameOver;// this will stop execution of drawgame method
+    // this will stop execution of drawgame method
+    return gameOver;
+}
+
+function displayGameOverText(snakes, Gameover)
+{
+    // Game Over Text
+    ctx.fillStyle="white";
+    ctx.font="50px verdana";
+    ctx.fillText("Game Over!", canvas.width/6.5, canvas.height/2);
+
+    // Winner Text
+    let winner;
+    let highestScore = 0;
+
+    let count = 0;
+    snakes.forEach(snake => {
+        if(snake.score !== count)
+        {
+            count++;
+        }
+    });
+
+    // Draw since all snakes have the same score (if 0 or 1)
+    if(count < 1)
+    {
+        ctx.fillStyle="white";
+    	ctx.font="36px verdana";
+    	ctx.fillText("Draw!", canvas.width/2.5, canvas.height/2 + 50);
+
+        return ga
+    }
+    // TODO: This will return only 1 winner even if multiple exist
+    else
+    {
+        snakes.forEach(snake => {
+            if(snake.score >= highestScore)
+            {
+                highestScore = snake.score;
+                winner = snake;
+            }
+        });
+
+        ctx.fillStyle=winner.headCol;
+    	ctx.font="36px verdana";
+    	ctx.fillText(winner.name + " Wins", canvas.width/5.5, canvas.height/2 + 50);
+    }
+}
+
+function wallCollision(snake, Gameover)
+{
+    if(snake.headX<0){//if snake hits left wall
+        Gameover=true;
+    }
+    else if(snake.headX===tileCount){//if snake hits right wall
+        Gameover=true;
+    }
+    else if(snake.headY<0){//if snake hits wall at the top
+        Gameover=true;
+    }
+    else if(snake.headY===tileCount){//if snake hits wall at the bottom
+        Gameover=true;
+    }
+}
+
+function wallTeleport(snake)
+{
+    if(snake.headX<0){//if snake hits left wall
+        snake.headX=tileCount;
+    }
+    else if(snake.headX===tileCount){//if snake hits right wall
+        snake.headX=0;
+    }
+    else if(snake.headY<0){//if snake hits wall at the top
+        snake.headY=tileCount;
+    }
+    else if(snake.headY===tileCount){//if snake hits wall at the bottom
+        snake.headY=0;
+    }
 }
 
 // score function
-function drawScore(){
-ctx.fillStyle=snake1HeadColor// set our text color to white
-ctx.font="10px verdena"//set font size to 10px of font family verdena
-ctx.fillText("Player1: " +score, 10,10);// position our score at right hand corner 
-}
+function drawScore(snake){
+    ctx.fillStyle = snake.headCol;
+    ctx.font= "10px verdena";
 
-// score2 function
-function drawScore2(){
-ctx.fillStyle=snake2HeadColor// set our text color to white
-ctx.font="10px verdena"//set font size to 10px of font family verdena
-ctx.fillText("Player2: " +score2, canvas.clientWidth-50, 10);// position our score at right hand corner 
+    if(snake.name.includes("1"))
+    {
+        ctx.fillText(snake.name + " : " + snake.score, canvasSize*2/100, 10);
+    }
+
+    if(snake.name.includes("2"))
+    {                                                   // 90%
+        ctx.fillText(snake.name + " : " + snake.score, canvasSize*30/100, 10);
+    }
+
+    if(snake.name.includes("3"))
+    {
+        ctx.fillText(snake.name + " : " + snake.score, canvasSize*65/100, 10);
+    }
+
+    if(snake.name.includes("4"))
+    {
+        ctx.fillText(snake.name + " : " + snake.score, canvasSize*90/100, 10);
+    }
 }
 
 // clear our screen
  function clearScreen(){
+    // make screen black
+    ctx.fillStyle= 'black';
+    // black color start from 0px left, right to canvas width and canvas height
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+ }
+ 
+ function drawSnake(snake){
+    
+    ctx.fillStyle=snake.BodyCol;
+    
+    //loop through our snakeparts array
+    for(let i=0;i<snake.parts.length;i++){
+        //draw snake parts
+        let part=snake.parts[i]
+         ctx.fillRect(part.x *tileCount, part.y *tileCount, tileSize,tileSize)
+    }
+    
+    //add parts to snake --through push
+    snake.parts.push(new snakePart(snake.headX,snake.headY));//put item at the end of list next to the head
+    
+    if(snake.parts.length>snake.tailLength){
+        snake.parts.shift();//remove furthest item from  snake part if we have more than our tail size
+    }
 
-ctx.fillStyle= 'black'// make screen black
-ctx.fillRect(0,0,canvas.clientWidth,canvas.clientHeight)// black color start from 0px left, right to canvas width and canvas height
+    ctx.fillStyle=snake.headCol;
+    ctx.fillRect(snake.headX* tileCount,snake.headY* tileCount, tileSize,tileSize)
  }
  
- function drawSnake(){
-    
-    ctx.fillStyle=snake1BodyColor;
-    
-    //loop through our snakeparts array
-    for(let i=0;i<snakeParts.length;i++){
-        //draw snake parts
-        let part=snakeParts[i]
-         ctx.fillRect(part.x *tileCount, part.y *tileCount, tileSize,tileSize)
-    }
-    
-    //add parts to snake --through push
-    snakeParts.push(new snakePart(headX,headY));//put item at the end of list next to the head
-    
-    if(snakeParts.length>tailLength){
-        snakeParts.shift();//remove furthest item from  snake part if we have more than our tail size
-    }
-    ctx.fillStyle=snake1HeadColor;
-    ctx.fillRect(headX* tileCount,headY* tileCount, tileSize,tileSize)
- }
- 
- function changeSnakePosition(){
-     headX=headX + xvelocity;
-     headY=headY+ yvelocity;
- }
- 
-  function drawSnake2(){
-    
-    ctx.fillStyle=snake2BodyColor;
-    
-    //loop through our snakeparts array
-    for(let i=0;i<snake2Parts.length;i++){
-        //draw snake parts
-        let part=snake2Parts[i]
-         ctx.fillRect(part.x *tileCount, part.y *tileCount, tileSize,tileSize)
-    }
-    
-    //add parts to snake --through push
-    snake2Parts.push(new snakePart(head2X,head2Y));//put item at the end of list next to the head
-    
-    if(snake2Parts.length>tail2Length){
-        snake2Parts.shift();//remove furthest item from  snake part if we have more than our tail size
-    }
-    ctx.fillStyle=snake2HeadColor;
-    ctx.fillRect(head2X* tileCount,head2Y* tileCount, tileSize,tileSize)
- }
- 
- function changeSnake2Position(){
-     head2X=head2X + xvelocity2;
-     head2Y=head2Y+ yvelocity2;
+ function moveSnake(snake){
+    snake.headX=snake.headX + snake.xVelocity;
+    snake.headY=snake.headY+ snake.yVelocity;
  }
  
  function drawApple(){
@@ -293,19 +315,12 @@ ctx.fillRect(0,0,canvas.clientWidth,canvas.clientHeight)// black color start fro
  }
  
  // check for collision and change apple position
- function checkCollision(){
-     if(appleX==headX && appleY==headY){
+ function checkCollision(snake){
+     if(appleX==snake.headX && appleY==snake.headY){
          appleX=Math.floor(Math.random()*tileCount);
          appleY=Math.floor(Math.random()*tileCount);
-         tailLength++;
-         score++; //increase our score value
-     }
-     
-     if(appleX==head2X && appleY==head2Y){
-         appleX=Math.floor(Math.random()*tileCount);
-         appleY=Math.floor(Math.random()*tileCount);
-         tail2Length++;
-         score2++; //increase our score value
+         snake.tailLength++;
+         snake.score++; //increase our score value
      }
  }
  
@@ -323,115 +338,130 @@ ctx.fillRect(0,0,canvas.clientWidth,canvas.clientHeight)// black color start fro
 
 function keyDown()
 {
+    //Arrows
 	// Arrow Up
     if(event.keyCode==38){
-    	moveUp2();
+    	moveUp(snake2);
     }
     
     // Arrow down
     if(event.keyCode==40){
-    	moveDown2();
+    	moveDown(snake2);
     }
 
 	// Arrow left
     if(event.keyCode==37){
-    	moveLeft2();
+    	moveLeft(snake2);
     }
     
     // Arrow right
     if(event.keyCode==39){
-    	moveRight2();
+    	moveRight(snake2);
     }
     
-    
+    //WASD
     // W - up
     if(event.keyCode==87){
-    	moveUp();
+    	moveUp(snake1);
     }
     
     // A - left
     if(event.keyCode==65){
-    	moveLeft();
+    	moveLeft(snake1);
     }
 
 	// S - down
     if(event.keyCode==83){
-    	moveDown();
+    	moveDown(snake1);
     }
     
     // D - right
     if(event.keyCode==68){
-    	moveRight();
+    	moveRight(snake1);
+    }
+
+    //Custom
+
+    //Custom1
+	//Up
+    if(event.keyCode==custom1[0]){
+    	moveUp(snake3);
+    }
+    
+    //Down
+    if(event.keyCode==custom1[1]){
+    	moveDown(snake3);
+    }
+
+	//Left
+    if(event.keyCode==custom1[2]){
+    	moveLeft(snake3);
+    }
+    
+    //Right
+    if(event.keyCode==custom1[3]){
+    	moveRight(snake3);
+    }
+
+    //Custom2
+	//Up
+    if(event.keyCode==custom2[0]){
+    	moveUp(snake4);
+    }
+    
+    //Down
+    if(event.keyCode==custom2[1]){
+    	moveDown(snake4);
+    }
+
+	//Left
+    if(event.keyCode==custom2[2]){
+    	moveLeft(snake4);
+    }
+    
+    //Right
+    if(event.keyCode==custom2[3]){
+    	moveRight(snake4);
     }
 }
 
-function moveUp()
+
+function moveUp(snake)
 {
-	if(yvelocity==1)
+	if(snake.yVelocity==1)
         return;
-        yvelocity=-1;
-        xvelocity=0;
+        snake.yVelocity=-1;
+        snake.xVelocity=0;
 }
 
-function moveDown()
+function moveDown(snake)
 {
-	if(yvelocity==-1)
+	if(snake.yVelocity==-1)
         return;
-        yvelocity=1;
-        xvelocity=0;
+        snake.yVelocity=1;
+        snake.xVelocity=0;
 }
 
-function moveRight()
+function moveRight(snake)
 {
-	if(xvelocity==-1)
+	if(snake.xVelocity==-1)
         return;
-        yvelocity=0;
-        xvelocity=1;
+        snake.yVelocity=0;
+        snake.xVelocity=1;
 }
 
-function moveLeft()
+function moveLeft(snake)
 {
-	if(xvelocity==1)
+	if(snake.xVelocity==1)
         return;
-        yvelocity=0;
-        xvelocity=-1;
-}
-
-function moveUp2()
-{
-	if(yvelocity2==1)
-        return;
-        yvelocity2=-1;
-        xvelocity2=0;
-}
-
-function moveDown2()
-{
-	if(yvelocity2==-1)
-        return;
-        yvelocity2=1;
-        xvelocity2=0;
-}
-
-function moveRight2()
-{
-	if(xvelocity2==-1)
-        return;
-        yvelocity2=0;
-        xvelocity2=1;
-}
-
-function moveLeft2()
-{
-	if(xvelocity2==1)
-        return;
-        yvelocity2=0;
-        xvelocity2=-1;
+        snake.yVelocity=0;
+        snake.xVelocity=-1;
 }
 
 document.addEventListener('touchstart', handleTouchStart, false);        
 document.addEventListener('touchmove', handleTouchMove, false);
 
+// Touch
 var xDown = null;                                                        
 var yDown = null;
 
@@ -458,15 +488,15 @@ function handleTouchMove(evt) {
                                                                          
     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
         if ( xDiff > 0 ) {
-           moveLeft(xvelocity, yvelocity);
+           moveLeft(snake.xVelocity, snake.yVelocity);
         } else {
-            moveRight(xvelocity, yvelocity);
+            moveRight(snake.xVelocity, snake.yVelocity);
         }                       
     } else {
         if ( yDiff > 0 ) {
-            moveUp(xvelocity, yvelocity);
+            moveUp(snake.xVelocity, snake.yVelocity);
         } else { 
-            moveDown(xvelocity, yvelocity);
+            moveDown(snake.xVelocity, snake.yVelocity);
         }                                                                 
     }
     /* reset values */
